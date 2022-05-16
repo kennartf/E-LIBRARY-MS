c1c916d393f8b8b32981005e4fbffde6322b2f83
kennartf <79041749+kennartf@users.noreply.github.com>
2022-05-16 13:37:12 -0500

    Create app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from E_learning import db
 if __name__ == "__main__":
     # db.create_all()
     # db.drop_all()
-    app.run(debug=False)
+    app.run(debug=True)
     # app.run(host='0.0.0.0', debug=True, port=5000)
 
 
